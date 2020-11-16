c2cf02ec29f5e7bdf32e5ff4f88ad9c40026fa7d
Filip Znaor <fznaor@etfos.hr>
2020-11-16 13:44:38 +0100

    Add Z1.py

---
+++ LV1/solutions/Z1.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 16 13:39:58 2020
+
+@author: student
+"""
+
+sati = float(input('Unesite broj radnih sati: '))
+cijena = float(input('Unesite zaradu po satu u kunama: '))
+print('Ukupno: %.2f kn' %(sati * cijena))
