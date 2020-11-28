0b7278766428b377eb7859960db1e0e9356e9910
Filip Znaor <fznaor@gmail.com>
2020-11-28 08:26:25 +0100

    Add zad_1

--- LV2/solutions/zad_1.py
+++ LV2/solutions/zad_1.py
@@ -6,7 +6,6 @@ file = open('../resources/mbox-short.txt')
 
 for line in file:
     emails = re.findall('[a-z0-9]+[\._]?[a-z0-9]+[@]\w+', line)
-    print(emails)
     for email in emails:
         email_names.append(email.split('@')[0])
     
